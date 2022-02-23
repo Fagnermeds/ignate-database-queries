4daacbae04641210bfcd33e4b3ad5371f24fab9f
fagnermeds <fagnermeds@gmail.com>
2022-02-22 21:42:58 -0300

    feat: add queries to complete tests

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -13,18 +13,29 @@ export class UsersRepository implements IUsersRepository {
 
   async findUserWithGamesById({
     user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
+  }: IFindUserWithGamesDTO): Promise<User | undefined> {
     // Complete usando ORM
+    const user = await this.repository.findOne(user_id, {
+      relations: ['games'],
+    });
+     
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const query = `
+      SELECT * FROM users AS u 
+      WHERE LOWER(u.first_name) = LOWER($1)
+      AND LOWER(u.last_name) = LOWER($2)
+    `;
+
+    return this.repository.query(query, [first_name, last_name]); // Complete usando raw query
   }
 }
